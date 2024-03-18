8fe5114af54cec2d335331c882358db2b43adf4a
Yenchai <163224501+Yenchai@users.noreply.github.com>
2024-03-19 00:22:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def handle_message(event):
     text1=event.message.text
     response = openai.ChatCompletion.create(
         messages=[
-            {"role": "teacher", "teaching": text1}
+            {"role": "teacher", "content": text1}
         ],
         model="gpt-3.5-turbo-0125",
         temperature = 0.5,
